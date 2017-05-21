945577454d438c7f6f87ca9d076e324a4a24e262
John Pope <jp@fieldstormapp.com>
2017-05-21 11:23:05 -0400

    use my stream reader with peak read line helper.

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
       .Package(url: "../ClangSwift", majorVersion: 0),
       .Package(url: "https://github.com/harlanhaskins/CommandLine.git", majorVersion: 3),
       .Package(url: "https://github.com/michael-yuji/CKit.git", Version(0,0,8)),
-      .Package(url: "https://github.com/hectr/swift-stream-reader.git", majorVersion:0)
+      .Package(url: "https://github.com/johndpope/swift-stream-reader.git", Version(0,2,1))
     ]
 )
