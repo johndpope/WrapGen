2527d1df11b477f21fbd5186e069ebc1e8c4ee44
John D. Pope 👽 <jp@fieldstormapp.com>
2017-05-22 20:47:52 -0400

    fixes.

--- RUNME.sh
+++ RUNME.sh
@@ -1,6 +1,15 @@
+# brew upgrade
+# brew doctor
+# fix any problems like this
+# You should change the ownership and permissions of these directories.
+# back to your user account.
+#  sudo chown -R $(whoami) /usr/local/include /usr/local/lib /usr/local/lib/pkgconfig
+# brew install llvm --force
+
 cd ..
 git clone https://github.com/trill-lang/ClangSwift
 cd ClangSwift
 swift utils/make-pkgconfig.swift
+cd ..
 cd WrapGen
 swift build
